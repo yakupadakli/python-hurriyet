21a6113f718196783066a23c6d25cd0365524aa8
Yakup Adakli <yakup.adakli@gmail.com>
2018-05-19 17:50:49 +0300

    setup.py added.

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import uuid
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+requirements = [str(req.req) for req in install_requirements]
+
+setup(
+    name="python-hurriyet",
+    version="1.0.0b1",
+    description="A Python client for the Hurriyet API.",
+    license="MIT",
+    author="Yakup Adaklı",
+    author_email="yakup.adakli@gmail.com",
+    url="http://github.com/yakupadakli/python-hurriyet.git",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=requirements,
+    keywords="hurriyet library",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    zip_safe=True,
+)
