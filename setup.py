1344edf62a3791398e27c380be27e35037b2e728
Yakup Adakli <yakup.adakli@gmail.com>
2018-05-19 17:59:10 +0300

    setup.py updated for pip version

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@
 import uuid
 
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
 requirements = [str(req.req) for req in install_requirements]
